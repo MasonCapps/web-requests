01f7256312d6d503987b2ef9e6ddd7c5db28c182
Mason Capps <masoncapps42119@gmail.com>
2022-04-30 00:01:25 -0500

    Updates files

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -7,15 +7,15 @@ while true
   if input_word.downcase == "q"
     break
   else
-    response_definition = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=o1tp029o4artgjis1rok72b3rxsv17su5kxnpee3fu4egy2ma")
+    response_definition = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
     definition_data = response_definition.parse(:json)
     definition = definition_data[1]["text"]
 
-    response_example = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/topExample?useCanonical=false&api_key=o1tp029o4artgjis1rok72b3rxsv17su5kxnpee3fu4egy2ma")
+    response_example = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/topExample?useCanonical=false&api_key=")
     example_data = response_example.parse(:json)
     example = example_data["text"]
 
-    response_pronunciation = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/pronunciations?useCanonical=false&limit=50&api_key=o1tp029o4artgjis1rok72b3rxsv17su5kxnpee3fu4egy2ma")
+    response_pronunciation = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/pronunciations?useCanonical=false&limit=50&api_key=")
     pronunciation_data = response_pronunciation.parse(:json)
     pronunciation = pronunciation_data[0]["raw"]
 
